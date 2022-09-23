08693a18221f186d3f891bc914a01fab39cb1333
Prithu Raj Banik <prithurb@gmail.com>
2022-09-23 17:04:01 +0600

    .

--- Phase Two/Pointers/main.cpp
+++ Phase Two/Pointers/main.cpp
@@ -1,1 +1,25 @@
+#include<iostream>
+#include<cmath>
+#include<algorithm>
+#include<vector>
+using namespace std;
+int main()
+{
+    int num = 5;
+    cout<<num<<endl;
 
+    // Adderess of operator --> & 
+    cout<<"Address of num is "<< &num <<endl; // hexadecimal form
+
+    int *ptr = &num;
+
+    // Dereference operator --> *
+    cout<<"Value of num is "<<*ptr<<endl;
+
+    //Printing address using pointer
+    cout<<"Printing address using pointer -->  "<< ptr <<endl;
+
+
+
+    return 0;
+}
