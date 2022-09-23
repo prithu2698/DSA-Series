f5a884672c7c0b74da243654ff768b543ed8bdfc
Prithu Raj Banik <prithurb@gmail.com>
2022-09-23 17:14:28 +0600

    .

--- Phase Two/Pointers/main.cpp
+++ Phase Two/Pointers/main.cpp
@@ -19,6 +19,11 @@ int main()
     //Printing address using pointer
     cout<<"Printing address using pointer -->  "<< ptr <<endl;
 
+    //Printing the size of pointer, address and value
+    cout<<"the size of value is "<< sizeof(num)<<endl;
+    cout<<"the size of value from the pointer is "<< sizeof(*ptr)<<endl;
+    cout<<"the size of address is "<< sizeof(ptr) <<endl;
+
 
 
     return 0;
