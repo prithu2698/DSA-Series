e0e5ad6839d89e2382e6243419f28ad270804559
Prithu Raj Banik <prithurb@gmail.com>
2022-09-30 01:10:15 +0600

    ;

--- Phase Two/1. Pointers/part 3/main.cpp
+++ Phase Two/1. Pointers/part 3/main.cpp
@@ -3,10 +3,21 @@
 #include<algorithm>
 #include<vector>
 using namespace std;
+
+void update(int **p){
+    // p = p + 1;
+    
+    // *p = *p + 1;
+
+    **p = **p + 1;
+    
+}
+
+
 int main()
 {
 
-
+/*
     //Double pointers
     int num = 5;
     int *i = &num;
@@ -22,7 +33,18 @@ int main()
     cout << k <<endl;
     cout << &k <<endl;
 
+*/
 
+    int p = 5;
+    int* p1 = &p;
+    int** p2 = &p1;
+    cout<< p<<endl;
+    cout<< p1<<endl;
+    cout<< p2<<endl;
+    update(p2);
+    cout<< p<<endl;
+    cout<< p1<<endl;
+    cout<< p2<<endl;
 
 
 
